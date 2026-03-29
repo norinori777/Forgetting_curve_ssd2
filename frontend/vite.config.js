558c98174545c58ffaecdd27fcd8429c358567a5
norinori777 <norinori6791@gmail.com>
2026-03-29 09:35:13 +0900

    外部端末からアクセスできるよう変更

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [react()],
     server: {
+        host: '0.0.0.0',
         proxy: {
             '/api': 'http://localhost:3000',
         },
